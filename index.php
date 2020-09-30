4245dfb09cc911f4647f5c1aaa128f57f4f42cb1
neveleneves <max.nikonov.work@gmail.com>
2020-09-30 21:48:54 +0300

    detailes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //Plugin name: AutoCalcPriceDocuments 
 //Author: Max Nikonov 
 //Author URI: https://github.com/neveleneves
-//Version: 1.0.0
+//Version: 1.0.1
 //Description: Auto price calculation for printing DOC/PDF documents
 
 //Load Script files
